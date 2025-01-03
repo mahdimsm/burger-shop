358bce3b4d954bfbc55289e6a30ede854dab2fec
mahdi sadeghi <mahdisadeghimsm1376@gmail.com>
2025-01-04 02:48:12 +0330

    imlement aos in sections

--- app/components/Features/Feature.tsx
+++ app/components/Features/Feature.tsx
@@ -16,7 +16,7 @@ const Feature = () => {
       <div className='w-[90%] md:w-[80%] mt-[3rem] md:mt-[5rem] mb-[3rem] mx-auto grid grid-cols-1
       md:grid-cols-2 lg:grid-cols-3 gap-[3rem]'>
         {/* 1st card */}
-        <div>
+        <div data-aos="fade-left" data-aos-anchor-placement="top-center">
             <div className='p-6 hover:bg-white rounded-lg transiton-all duration-200'>
                 <Image src={Img1} alt="burger" className='rounded-3xl'/>
                 <h1 className='mt-[1.5rem] text-center text-[24px] text-black font-semibold'>Our Burger</h1>
@@ -24,7 +24,7 @@ const Feature = () => {
             </div>
         </div>
         {/* 2nd card */}
-        <div>
+        <div data-aos="zoom-in" data-aos-delay="200" data-aos-anchor-placement="top-center">
             <div className='p-6 hover:bg-white rounded-lg transiton-all duration-200 lg:translate-y-[3rem]'>
                 <Image src={Img2} alt="burger" className='rounded-3xl'/>
                 <h1 className='mt-[1.5rem] text-center text-[24px] text-black font-semibold'>Your Opinion is Important</h1>
@@ -32,7 +32,7 @@ const Feature = () => {
             </div>
         </div>
         {/* 3rd card */}
-        <div>
+        <div data-aos="fade-right" data-aos-delay="400" data-aos-anchor-placement="top-center">
             <div className='p-6 hover:bg-white rounded-lg transiton-all duration-200'>
                 <Image src={Img3} alt="burger" className='rounded-3xl'/>
                 <h1 className='mt-[1.5rem] text-center text-[24px] text-black font-semibold'>Chicken Burger</h1>
