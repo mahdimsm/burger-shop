82054d8d92b518eafa1a35532c8cf35ad65ec48e
mahdi sadeghi <mahdisadeghimsm1376@gmail.com>
2025-01-04 02:36:46 +0330

    initial setting for AOS

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client'
+import React, { useEffect } from 'react'
 import Hero from './Hero/Hero'
 import Feature from './Features/Feature'
 import PopularBurger from './PopularBurger/PopularBurger'
@@ -6,8 +7,23 @@ import Delivery from './Delivery/Delivery'
 import Team from './Team/Team'
 import Reservation from './Reservation/Reservation'
 import Newsletter from './Newsletter/Newsletter'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
 
 const Home = () => {
+
+  useEffect(()=>{
+      const initAOS = async ()=>{
+        await import('aos');
+        AOS.init({
+          duration:1000,
+          easing:'ease',
+          once:true,
+          anchorPlacement:"top-center",
+        })
+      }
+      initAOS();
+  },[])
   return (
     <div className='overflow-hidden'>
       <Hero/>
